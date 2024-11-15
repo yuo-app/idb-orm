49cf8657a992150724c5ef63ee946720335f825a
Hegyi Áron Ferenc <36823200+Rettend@users.noreply.github.com>
2024-11-15 19:49:02 +0100

    fix: everything

--- packages/idb-orm/build.config.ts
+++ packages/idb-orm/build.config.ts
@@ -1,6 +1,12 @@
+import fs from 'node:fs'
 import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
+  hooks: {
+    'build:done': () => {
+      fs.copyFileSync('../../README.md', './README.md')
+    },
+  },
   entries: [
     'index',
     'src',
