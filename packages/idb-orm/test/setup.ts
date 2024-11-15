6a4d18069a2eb56b9e318fb996dcd50e926f1d82
Hegyi Áron Ferenc <36823200+Rettend@users.noreply.github.com>
2024-11-15 18:45:13 +0100

    ci: uh no idb in gh actions

--- packages/idb-orm/test/setup.ts
+++ packages/idb-orm/test/setup.ts
@@ -1,3 +1,15 @@
+import { JSDOM } from 'jsdom'
 import { setup } from 'vitest-indexeddb'
 
 setup()
+
+if (!global.window) {
+  const dom = new JSDOM('<!DOCTYPE html><html><body></body></html>', {
+    url: 'http://localhost',
+  })
+  global.window = dom.window as any
+  global.document = dom.window.document
+}
+
+if (!global.indexedDB)
+  global.indexedDB = window.indexedDB
